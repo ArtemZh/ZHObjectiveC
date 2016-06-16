33d994f5370f9e45e6e0d8394552bc03d71626e3
Artem Zhavrotskiy <artemiyzh@gmail.com>
2016-06-16 20:15:22 +0300

    Add ZHCreatureTest.h

---
+++ ZHCreatureTest.h
@@ -0,0 +1,13 @@
+//
+//  ZHCreatureTest.h
+//  ZHObjectiveC
+//
+//  Created by Artem Zhavrotskiy on 16.06.16.
+//  Copyright © 2016 Artem Zhavrotskiy. All rights reserved.
+//
+
+#ifndef ZHCreatureTest_h
+#define ZHCreatureTest_h
+
+
+#endif /* ZHCreatureTest_h */
---
+++ ZHObjectiveC/Tests/ZHCreatureTest.m
@@ -0,0 +1,9 @@
+//
+//  ZHCreatureTest.m
+//  ZHObjectiveC
+//
+//  Created by Artem Zhavrotskiy on 16.06.16.
+//  Copyright © 2016 Artem Zhavrotskiy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
